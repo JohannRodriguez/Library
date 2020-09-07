b83f57b248004833ec04dd92a28683ef7609c7ca
JohannRodriguez <54152734+JohannRodriguez@users.noreply.github.com>
2020-09-07 16:36:36 -0600

    Coorect rubocop errors

--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,4 @@
-let myLibrary = JSON.parse(localStorage.getItem("library")) || [];
+const myLibrary = JSON.parse(localStorage.getItem('library')) || [];
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -9,39 +9,38 @@ function Book(title, author, pages, read) {
 
 function addBookToLibrary(title, author, pages, read) {
   myLibrary.push(new Book(title, author, pages, read));
-  localStorage.setItem("library", JSON.stringify(myLibrary));
-  alert("Book was succesfully added")
+  localStorage.setItem('library', JSON.stringify(myLibrary));
 }
 
 // Add new book with user inputs
-let addBookBtn = document.getElementById("add-book-btn");
-addBookBtn.addEventListener("click", () => {
-  let title = document.getElementById("title").value;
-  let author = document.getElementById("author").value;
-  let pages = document.getElementById("pages").value;
-  let readCheckBox = document.getElementById("read");
+const addBookBtn = document.getElementById('add-book-btn');
+addBookBtn.addEventListener('click', () => {
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  const pages = document.getElementById('pages').value;
+  const readCheckBox = document.getElementById('read');
 
-  if(readCheckBox.checked) {
+  if (readCheckBox.checked) {
     var read = true;
   } else {
     var read = false;
   }
 
-  if (title == "" || author == "" || pages == "") {
-    return alert("All fields must be filled")
+  if (title === '' || author === '' || pages === '') {
+    return alert('All fields must be filled');
   };
 
   addBookToLibrary(title, author, pages, read);
 });
 
 // Button to hide and show the new book form
-let showForm = document.getElementById("new-book-form");
+const showForm = document.getElementById('new-book-form');
 
-showForm.addEventListener("click", function() {
-  let bookForm = document.getElementById("book-form");
-  if (bookForm.style.display == "none") {
-    bookForm.style.display = "block";
+showForm.addEventListener('click', () => {
+  const bookForm = document.getElementById('book-form');
+  if (bookForm.style.display === 'none') {
+    bookForm.style.display = 'block';
   } else {
-    bookForm.style.display = "none";
+    bookForm.style.display = 'none';
   };
 });
